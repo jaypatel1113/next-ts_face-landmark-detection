48d41a25ada32296700773e739ed7bb8e3833137
Jay Patel <jay64441860@gmail.com>
2024-03-19 20:24:14 +0530

    added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ interface InputResolution {
 
 const inputResolution: InputResolution = {
     width: 1080,
-    height: 900,
+    height: 750,
 };
 
 const videoConstraints: WebcamProps["videoConstraints"] = {
@@ -47,7 +47,8 @@ const App: React.FC = () => {
             <Webcam
                 width={inputResolution.width}
                 height={inputResolution.height}
-                style={{ visibility: "hidden", position: "absolute" }}
+                // style={{ visibility: "hidden", position: "absolute" }}
+                style={{ position: "absolute" }}
                 videoConstraints={videoConstraints}
                 onLoadedData={handleVideoLoad}
             />
@@ -63,40 +64,3 @@ const App: React.FC = () => {
 };
 
 export default App;
-
-
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
